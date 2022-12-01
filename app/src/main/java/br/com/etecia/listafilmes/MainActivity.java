f0c1a83b4c9ef52ff4075cb4faf24ebddcb3eb6b
NathanMaced <nathanmacedo02@gmail.com>
2022-12-01 19:50:49 -0300

    Criação de lista (ListView)

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.listafilmes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
         //preparar para instanciar o adaptador
         listaFilmes.setAdapter(Adaptador);
+    }
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
