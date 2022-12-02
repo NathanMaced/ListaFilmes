09efa28a270c8d8a8c4387af94c205ff89b73301
NathanMaced <nathanmacedo02@gmail.com>
2022-12-01 21:58:34 -0300

    Ligando a base de dados

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -13,6 +13,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     ListView listaFilmes;
 
+    //CRIAR A BASE DE DADOS PARA OS FILMES
+    String titulo[] = {"Bob Esponja", "Patrulha Canina", "Hotel Transilvânia", "Pinguins de Madagascar", "Sing 2", "Sonic 2"};
+    String descricao[] = {getString(R.string.bob_esponja),
+            getString(R.string.patrulha_canina),
+            getString(R.string.hotel_transilvania),
+            getString(R.string.pinguins_madagascar),
+            getString(R.string.sing),
+            getString(R.string.sonic)
+    };
+    String acesso[] = {"4.5", "4.0", "5.0", "2.0", "3.7", "1.0"};
+    int filmes[] = {R.drawable.bobesponja, R.drawable.patrulha, R.drawable.hotel, R.drawable.pinguin, R.drawable.sing2, R.drawable.sonic};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +35,14 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter adapter = new MyAdapter();
 
         //preparar para instanciar o adaptador
-        listaFilmes.setAdapter(Adaptador);
+        //listaFilmes.setAdapter(Adaptador);
     }
-    public class MyAdapter extends BaseAdapter{
+
+    public class MyAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return 0;
+            return filmes.length;
         }
 
         @Override
@@ -51,12 +64,19 @@ public class MainActivity extends AppCompatActivity {
 
             View v = getLayoutInflater().inflate(R.layout.modelo_filmes, null);
 
+            //APRESENTANDO AS VARIAVEIS DO JAVA PARA O XML
             imagemFilme = v.findViewById(R.id.idImagemFilme);
             tituloFilme = v.findViewById(R.id.idTituloFilme);
             descricaoFilme = v.findViewById(R.id.idDescricaoFilme);
             avaliacao = v.findViewById(R.id.idAvaliacao);
 
-            return null;
+
+            //inserindo valores nas variaveis
+            tituloFilme.setText(titulo[i]);
+            descricaoFilme.setText(descricao[i]);
+            imagemFilme.setImageResource(filmes[i]);
+            avaliacao.setText(acesso[i]);
+            return v;
         }
     }
 }
